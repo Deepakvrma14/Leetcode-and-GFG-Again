92e021b9321fdf8bd50e1e47a5a6add69735e0ff
Deepakvrma14 <86903293+Deepakvrma14@users.noreply.github.com>
2023-09-19 22:28:56 +0530

    Create fuind duplixcates 2 approaches.cpp

---
+++ fuind duplixcates 2 approaches.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+     
+    int findDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        for(int  i : nums){
+            int index = abs(i);
+            if(nums[index]<0){
+                return index;
+            }
+            nums[index] = -nums[index];
+        }
+        return n ;
+    }
+};
+
